7274424f406ab08abaf5bc831f8606b6e312daf1
wander1405 <alexander.cabnal.1405@gmail.com>
2023-08-21 08:21:31 -0600

    primer commit

---
+++ includes/Includes/title.php
@@ -0,0 +1,1 @@
+  <title>CodeMinds</title>
---
+++ includes/conn.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$user = "root" /*"id20936476_root"*/;
+$pass = ""/*"Wander1405."*/;
+$db = "gestion_estudiantes"/* "id20936476_gestion_estudiantes"*/;
+
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+    die("No se pudo conectar a la base de datos: " . $conn->connect_error);
+}   
+?>
